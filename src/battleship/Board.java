698be4cbd8e1f8284838c4aa5778c439979e503c
wf-lee <weifan2647@gmail.com>
2018-06-20 23:33:43 +0800

    clean up class files

---
+++ src/battleship/Board.java
@@ -0,0 +1,14 @@
+
+// Board consists of 20 rows x 60 column
+
+public class Board extends Game {
+	private String testString;
+	public Board() {
+		testString = "Hello World";
+	}
+
+	public String getString() {
+		return testString;
+	}
+	
+}
---
+++ src/battleship/Game.java
@@ -0,0 +1,15 @@
+
+
+public class Game {
+
+	public static void main(String[] args) {
+		// System.out.print("hello world");
+		Board b1 = new Board();
+
+		System.out.println(b1.getString());
+		
+		
+
+	}
+
+}
