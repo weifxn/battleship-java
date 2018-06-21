c2e53c039def85be1a49444d317e061f56905bbc
wf-lee <weifan2647@gmail.com>
2018-06-21 23:49:45 +0800

    add exceptionhandling

--- src/battleship/Game.java
+++ src/battleship/Game.java
@@ -6,6 +6,7 @@ Changelog:
 
 */
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class Game {
@@ -13,6 +14,7 @@ public class Game {
 		int choice = 1;
 		while (choice > 0) {
 			Board b1 = new Board();
+			String[] test = "abc".split("");
 			System.out.println(b1.displayBoard());
 			choice = getUserChoice();
 			clearScreen();
@@ -22,10 +24,23 @@ public class Game {
 	}
 	// get user choice
     private static int getUserChoice() {
-        Scanner input = new Scanner( System.in );
-        System.out.println( "Enter location to boom (row, col): " );
-        return input.nextInt();
+		int choice = 1;
+		while (choice > 0) {
+			try {
+				Scanner input = new Scanner( System.in );
+				System.out.println( "Enter row: " );
+				choice = input.nextInt();
+				System.out.println( "Enter column: " );
+				choice = input.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Please enter integer.");
+			}
+		}
+		
+		return choice;
 	} 
+
 	
 	public static void clearScreen() {  
 		System.out.print("\033[H\033[2J");  
