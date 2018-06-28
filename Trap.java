f9a66c8a7e65c0fe2879a33c98d8906d08577322
pat922 <sooding097@gmail.com>
2018-06-28 19:57:20 +0800

    trap is execute

--- Trap.java
+++ Trap.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Trap extends Entity {
     public Trap( Player player ) {
         super(player);
@@ -5,8 +7,24 @@ public class Trap extends Entity {
 
     @Override
     public void execute() {
-    	System.out.println("You hit Trap!");
+    	Player player = new Player();
+    	String highlow = "";
+    	Random rand = new Random();
+    	int dangerlevel=rand.nextInt(2);
+    	if (dangerlevel==0) {
+    		highlow = "low";
+    		player.reduceLives();
+    		System.out.println(player.getLives());
+    	}
+    	else {
+    		highlow = "high";
+    		player.reduceLives();
+    		player.reduceLives();
+    		System.out.println(player.getLives());
+    	}
     	
-        
+    	System.out.println("You hit " + highlow + " Trap!");
     }
+    
+    
 }
