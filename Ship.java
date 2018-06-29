cf3f3ca489516eed991f44d7715b5963ded6ea15
wf-lee <weifan2647@gmail.com>
2018-06-29 19:19:53 +0800

    simplify ship populate

--- Ship.java
+++ Ship.java
@@ -21,51 +21,35 @@ public class Ship extends Entity {
         System.out.println("ship no: "+shipno);
 		int count = 0;
 		while (count < shipno) {
-			int  row = rand.nextInt(19);
-			int  col = rand.nextInt(59);
+			int  row = rand.nextInt(20);
+			int  col = rand.nextInt(60);
 			boolean gotShip = false;
 
-			int length =  rand.nextInt(3) + 3;
-			if (col < 54) {
-				for (int i = col; i < col + (length + 1); i++){
-					if (map[row][i] == 1) {
-						gotShip = true;
+            int length =  rand.nextInt(3) + 3;
+            
+            // row 0 and col 60 wont have ship
+            // fix this
+			if (col < 54 && col > 0) {
+                if (map[row][col+(length+1)] == 0) {
+                    if col > 0
+                    map[row][col-1] == 0
+                    for (int i = col; i < col + length; i++){
+						map[row][i] = 1;
 					}
-				}// after length + 1
-				if (col > 0 ){
-					if (map[row][col-1] == 1) {
-						gotShip = true;
-					}
-				}
-				if(!gotShip) {
-					for (int i = col; i < col + length; i++){
+                }
+			}
+			else if (col > 53 && col < 60) {
+                if(map[row][col-(length+1)] == 0
+                && map[row][col+1] == 0){
+                    for (int i = col; i > col - length ; i--){
 						map[row][i] = 1;
 					}
 				}
-			}
-			else {
-				for (int i = col; i > col - (length + 1) ; i--){
-					if (map[row][i] == 1) {
-						gotShip = true;
-					}
-				}// before length + 1
-				if (col < 59){
-					if (map[row][col+1] == 1) {
-						gotShip = true;
-					}
-				}
-				if (!gotShip){
-					for (int i = col; i > col - length ; i--){
-						map[row][i] = 1;
-					}
-				}
-				
-			}
+            }
+            else {
+                count--;
+            }
 			count++;
-			if (gotShip) {
-				gotShip = false;
-				count -=1;
-			}
         }
         return map;
 	}
