e4e4799fbcda514ae7e1ae0fce09f73bdb0da752
wf-lee <weifan2647@gmail.com>
2018-07-03 17:53:46 +0800

    nothing

--- Game.java
+++ Game.java
@@ -36,7 +36,7 @@ Changelog:
 Fix: 
 
 */
-
+import java.io.File;
 
 public class Game {
 	// classes
@@ -119,7 +119,35 @@ public class Game {
 			}
 			else if (exit == 4) {
 				System.out.println("Congratulations!Please enter your name:");
-
+				File f = null;
+				boolean bool = false;
+				
+				try {
+				
+				   // create new file
+				   f = new File("test.txt");
+				   
+				   // tries to create new file in the system
+				   bool = f.createNewFile();
+				   
+				   // prints
+				   System.out.println("File created: "+bool);
+				   
+				   // deletes file from the system
+				   f.delete();
+				   
+				   // delete() is invoked
+				   System.out.println("delete() method is invoked");
+				   
+				   // tries to create new file in the system
+				   bool = f.createNewFile();
+				   
+				   // print
+				   System.out.println("File created: "+bool);
+					  
+				} catch(Exception e) {
+				   e.printStackTrace();
+				}
 			}
 			grid.updateGrid();
 			player.addSteps();
